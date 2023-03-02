c7695c4d2b6fa7ddc3f8b1089b0f92ed3d787b81
martha <solomonmartha50@gmail.com>
2023-03-02 06:22:42 -0800

    reverse_array

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: if  str1 < str2, the negative difference
+ * if str1 == str2, 0.
+ * if str1 > str2, the positive difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
