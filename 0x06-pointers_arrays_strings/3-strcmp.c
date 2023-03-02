73d42ef3a6257839f83821c2046810e788206040
martha <solomonmartha50@gmail.com>
2023-03-02 07:31:41 -0800

    strncpy

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,16 +3,20 @@
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
- * Return: if  str1 < str2, the negative difference
- * if str1 == str2, 0.
- * if str1 > str2, the positive difference
+ * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	return (*s1 - *s2);
+	return (0);
 }
