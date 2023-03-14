e3535b9356e4fef89cb9e39422ae769f80b3fc85
martha <solomonmartha50@gmail.com>
2023-03-14 12:55:06 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -60,15 +60,15 @@ char **strtow(char *str)
 	if (words == 0)
 		return (NULL);
 	strings = malloc(sizeof(char *) * (words + 1));
-	if (string == NULL)
+	if (strings == NULL)
 		return (NULL);
 	for (w = 0; w < words; w++)
 	{
 		while (str[index] == ' ')
 			index++;
 		letters = word_len(str + index);
-		strings[w] = malloc(sizeof(char) * (letters + 1))
-			if (string[w] == NULL)
+		strings[w] = malloc(sizeof(char) * (letters + 1));
+		if (strings[w] == NULL)
 			{
 				for (; w >= 0; w--)
 					free(strings[w]);
@@ -76,8 +76,8 @@ char **strtow(char *str)
 				return (NULL);
 			}
 		for (l = 0; l < letters; l++)
-			strings[w][l] = str[index++]
-			strings[w][l] = '\0';
+			strings[w][l] = str[index++];
+		strings[w][l] = '\0';
 	}
 	strings[w] = NULL;
 	return (strings);
