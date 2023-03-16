1b0ab4f1be980de4c620c4d2e0c9e051a5536fec
martha <solomonmartha50@gmail.com>
2023-03-15 19:11:09 -0700

    create static lib

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _isalpha - check for alphabetic
+ * @c: character
+ * Return: i if letter, 0 if not
+int _isalpha(int c)
+{
+	return _isalpha(c);
+}
